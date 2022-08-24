56801ae59195e4a95cc3b43c3a8cf3ed27c27f5e
Chinzorig Otgonjargal <chinzorig.o@gmail.com>
2022-08-24 22:44:12 +0800

    Signup

--- react-frontend-01-starting-setup/src/App.js
+++ react-frontend-01-starting-setup/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
+import Auth from './user/pages/Auth';
 import UpdatePlace from './places/pages/UpdatePlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
@@ -25,6 +26,9 @@ const App = () => {
           </Route>
           <Route path="/places/:placeId">
             <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
           </Route>
           <Redirect to="/" />
       </Switch>
---
+++ react-frontend-01-starting-setup/src/user/pages/Auth.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "../../shared/components/UIElements/Card";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
+
+import "./Auth.css";
+
+const Auth = () => {
+    const [formState, inputHandler] = useForm({
+        email: {
+            value: '',
+            isValid: false
+        },
+        password: {
+            value: '',
+            isValid: false
+        }
+    }, false);
+
+  return (
+    <Card className="authentication">
+      <h2>Login required</h2>
+      <hr />
+      <form>
+        <Input
+          element="input"
+          id="email"
+          type="email"
+          label="E-mail"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address!"
+          onChange={inputHandler}
+        />
+      </form>
+    </Card>
+  );
+};
+
+export default Auth;
